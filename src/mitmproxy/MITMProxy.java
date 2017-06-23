3077cf43d7e7774145393dfe461538530910aeb4
Gogs <gogs@fake.local>
2017-06-23 15:43:44 -0500

    Added home page.

--- src/mitmproxy/MITMProxy.java
+++ src/mitmproxy/MITMProxy.java
@@ -83,7 +83,6 @@ class ProxyRule {
 						} catch (Exception e) {
 							rule.replacementString = "MITMProxy-NO-INJECT-FILE-FOUND";
 						}
-						
 					}
 					if (rules != null) {
 						rules.add(rule);
@@ -93,7 +92,6 @@ class ProxyRule {
 				}
 			}
 		}
-		
 		// GET ALL RULES
 		ArrayList<ProxyRule> allRules = new ArrayList<ProxyRule>();
 		if (fileRules != null) {
@@ -545,7 +543,6 @@ class DirectoryCache implements Iterable<Entry<String, ByteBuffer>> {
 
 class ProxyStaticWebHandler {
 	private static String commonHeaders = 	"Server: MITMProxy Server\r\n"
-											+ "Content-Type: text/html\r\n"
 											+ "Connection: Closed\r\n";	
 	private String root = "";
 	private DirectoryCache fileCache;
@@ -554,10 +551,11 @@ class ProxyStaticWebHandler {
 		fileCache = new DirectoryCache(this.root);
 	}
 		
-	private static String getHeaders(int status, int contentLength) {
+	private static String getHeaders(int status, int contentLength, String contentType) {
 		String headers = commonHeaders;
 		if (status == 200) {
 			headers = "HTTP/1.1 200 OK\r\n" + headers;
+			headers = headers + "Content-Type: "+contentType+"\r\n";
 			headers = headers + "Conetne-Length: "+contentLength+"\r\n\r\n";
 		} else {
 			headers = "HTTP/1.1 404 Not Found\r\n" + headers;
@@ -568,19 +566,27 @@ class ProxyStaticWebHandler {
 	public void handle(Socket socket, ProxyRequestEditor request) throws IOException {
 		String processedPath = request.getProcessedPath();
 		// Home
-		if (processedPath.compareTo("/") == 0) {
+		if (processedPath == null || processedPath.compareTo("/") == 0) {
 			processedPath = "/index.html";
 		}
 		// strip first "/" since file caching is based on system paths.
 		processedPath = processedPath.replaceFirst("\\/", "");
+		String contentType = "text/html";
+		if (processedPath.matches(".*\\.html") || processedPath.matches(".*\\.html")) {
+			contentType = "text/html";
+		} else if (processedPath.matches(".*\\.css")) {
+			contentType = "text/css";
+		} else if (processedPath.matches(".*\\.js")) {
+			contentType = "application/javascript";
+		}
 		byte[] responseBodyBytes = fileCache.getFileBytes(processedPath);
 		if (responseBodyBytes == null) {
-			String headers = getHeaders(404, 0);
+			String headers = getHeaders(404, 0, contentType);
 			byte[] responseHeadersBytes = headers.getBytes(StandardCharsets.US_ASCII);
 			OutputStream clientResponseStream = socket.getOutputStream(); 
 			clientResponseStream.write(responseHeadersBytes);
 		} else {
-			String headers = getHeaders(200, responseBodyBytes.length);
+			String headers = getHeaders(200, responseBodyBytes.length, contentType);
 			byte[] responseHeadersBytes = headers.getBytes(StandardCharsets.US_ASCII);
 			byte[] both = new byte[responseHeadersBytes.length + responseBodyBytes.length]; 
 			System.arraycopy(responseHeadersBytes, 0, both,                           0, responseHeadersBytes.length);
